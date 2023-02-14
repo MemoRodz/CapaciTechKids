ccad5e24f330367d7a2bafc56be9fe99398e91ce
Sandro <sandro_malca@usmp.pe>
2023-02-13 20:46:44 -0500

    add comentario hola mundo

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>HOLA MUNDO</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
