94fe72790b66a2e710a3c4bb1d7001b76ea08386
Fabian Rizzi <faberizzi92@gmail.com>
2023-02-15 19:32:55 +0100

    Donation component created

---
+++ src/pages/Donate/Donate.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Donate() {
+  return (
+    <div>
+    <h1>Donation page</h1>
+    <h3>Thanks for your donations.</h3>
+    <p>Donations are used to pay for the server costs and to keep the site running.</p>
+    
+    
+    <button>DONATE</button>
+    
+    </div>
+  )
+}
+
+export default Donate
