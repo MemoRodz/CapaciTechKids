3bf56bc4ac6f2bfe13848e30c45073723c4fb18e
Giuliana Schiebelbein <giulianaschiebelbein2@gmail.com>
2023-02-16 20:18:04 -0300

    styles home

--- src/component/Layout/Nav/Nav.jsx
+++ src/component/Layout/Nav/Nav.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import styles from './nav.module.css'
 import { Link } from 'react-router-dom'
 function Nav() {
   return (
-    <>
-        <nav>
-            <ul>
-                <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"/course"}>My Course</Link></li>
-                <li><Link to={"/create"}>Create Course</Link></li>
-                <li><Link to={"/donate"}>Donate</Link></li>
-                <li><Link to={"/about"}>About</Link></li>
-            </ul>
-        </nav>
-    </>
+
+    <div className={styles.menu}>
+      <nav>
+        <ul>
+          <li><Link to={"/"}>Home</Link></li>
+          <li><Link to={"/course"}>My Course</Link></li>
+          <li><Link to={"/create"}>Create Course</Link></li>
+          <li><Link to={"/donate"}>Donate</Link></li>
+          <li><Link to={"/about"}>About</Link></li>
+        </ul>
+      </nav>
+    </div>
+
   )
 }
 
