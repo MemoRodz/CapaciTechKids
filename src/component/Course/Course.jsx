95cb900f94e3023f4ad7d467d3951107636cdd60
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-02-16 12:15:49 -0300

    realizando styles en Course

--- src/component/Course/Course.jsx
+++ src/component/Course/Course.jsx
@@ -1,45 +1,50 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Estrella from '../Estrella/Estrella';
 import style from '../course/Course.module.css'
 export default function Course(props) {
 
-    const {Title, Description, Category,Image,score}=props
+  const { Title, Description, Category, Image, score } = props
 
-    const [isFav, setIsFav] = useState(false);
-    //const dispatch = useDispatch();
-    //const myFavorites = useSelector((s) => s.myFavorites);
+  const [isFav, setIsFav] = useState(false);
+  //const dispatch = useDispatch();
+  //const myFavorites = useSelector((s) => s.myFavorites);
 
 
-    function handleFavorite() {
-        if (isFav) {
-        setIsFav(false);
-        //dispatch(deleteFavorites(ch.id));
-        } else {
-        setIsFav(true);
-        //dispatch(addFavorites(ch));
-        }
+  function handleFavorite() {
+    if (isFav) {
+      setIsFav(false);
+      //dispatch(deleteFavorites(ch.id));
+    } else {
+      setIsFav(true);
+      //dispatch(addFavorites(ch));
     }
+  }
 
 
   return (
     <div className={style.cont}>
-        <div className={style.upbar_card}>
-            {isFav ? (
-                <button onClick={() => handleFavorite()}>❤️</button>
-                ) : (
-                <button onClick={() => handleFavorite()}>🤍</button>
-            )}
-           <button className={style.bttn} onClick={props.onClose}>X</button>
-        </div> 
-        
-        <div>
-                <img className={style.img} src={Image} alt={Image}/>
-                <h3>{Title}</h3>
-                <h4>{Category.slice(" ")}</h4>
-                <Estrella score={score} />
-                <h4>Descripcion: <br/>{Description}</h4>
-                
-                </div>                                          
+      <div className={style.left}>
+        <img className={style.img} src={Image} alt={Image} />
+      </div>
+
+      <div className={style.right}>
+      <div className={style.upbar_card}>
+        {isFav ? (
+          <button onClick={() => handleFavorite()}>❤️</button>
+        ) : (
+          <button onClick={() => handleFavorite()}>🤍</button>
+        )}
+        <button className={style.bttn} onClick={props.onClose}>X</button>
+      </div>
+
+      <div className={style.inf}>
+        <h1>{Title}</h1>
+        <span>{Category.slice(" ")}</span>
+        <Estrella score={score} />
+        <p>Descripcion: <br />{Description}</p>
+      </div>
+      </div>
+
     </div>
   )
 }
