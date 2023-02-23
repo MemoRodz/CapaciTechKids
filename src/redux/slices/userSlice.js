b485b634e2e72d1aead91a78a986eabc6f5e5137
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-02-23 12:08:58 -0300

    Update userSlice.js

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    isLogged: false,
+    isLogged: true,
     userRole: '',
     userName: '',
 }
