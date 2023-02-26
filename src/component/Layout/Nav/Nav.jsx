0186c84e2be718dc237b7d7e50e9cd3771e91f3a
Ricardo Ariel Maya <rickymayita@hotmail.com>
2023-02-26 09:10:51 -0300

    css en home

--- src/component/Layout/Nav/Nav.jsx
+++ src/component/Layout/Nav/Nav.jsx
@@ -1,7 +1,15 @@
 import { NavLink, useLocation } from 'react-router-dom'
 import styles from './Nav.module.css'
+import { useAuth0 } from '@auth0/auth0-react';
+import { LoginButton, LogoutButton, Profile } from '../../../component'
 
 function Nav() {
+
+  const { isAuthenticated, user } = useAuth0();
+
+  user && console.log(user);
+
+
   const location = useLocation()
 
 
@@ -27,7 +35,19 @@ function Nav() {
           <li><NavLink to={"/donate"} style={({isActive}) => isActive ? activeStyle : inactiveStyle}>Donaciones</NavLink></li>
           <li><NavLink to={"/about"} style={({isActive}) => isActive ? activeStyle : inactiveStyle}>Nosotros</NavLink></li>
           {/* <li><NavLink to={"/about"} style={({isActive}) => isActive ? activeStyle : inactiveStyle}>About</NavLink></li> */}
-          
+          <div className={styles.login}>
+
+            {isAuthenticated ? <>
+              <Profile />
+              <LogoutButton />
+            </>
+              :
+              <>
+                <LoginButton />
+              </>
+            }
+
+          </div>
         </ul>
       </nav>
     </>
