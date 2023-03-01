e1add8301e457d3a55401a175b9a987246696f87
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-03-01 17:47:21 -0300

    Image and name en perfiles de usuarios

--- src/pages/Student/StudentProfile/StudentProfile.jsx
+++ src/pages/Student/StudentProfile/StudentProfile.jsx
@@ -1,13 +1,14 @@
+import { useSelector } from 'react-redux'
 import styles from './StudentProfile.module.css'
 
 function StudentProfile() {
+    const userInfo = useSelector(state => state.user)
 
     return (
         <>
-            <div className={styles.student_container}>
-                <div className={styles.student_sidebar}>
-
-                </div>
+            <div className={styles.student_profile}>
+                <img src={userInfo.Image ? userInfo.Image : "Image Not Found"} alt="profile-image" height={200} width={200} />
+                <h2>{userInfo.Name ? userInfo.Name : "Name?"}</h2>
             </div>
         </>
     )
