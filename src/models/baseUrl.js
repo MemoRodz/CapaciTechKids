b2ac9ef2dde0091f87ec7490ab849c801e572f40
MemoRodz <33233688+MemoRodz@users.noreply.github.com>
2023-03-03 17:21:16 -0600

    Update baseUrl.js

--- src/models/baseUrl.js
+++ src/models/baseUrl.js
@@ -1,2 +1,2 @@
 // local url = 'http://localhost:3001' / deploy url = ??? 
-export const baseUrl = 'https://capacitechkidsback-production-2437.up.railway.app/'
+export const baseUrl = 'https://capacitechkidsback-production-2437.up.railway.app'
