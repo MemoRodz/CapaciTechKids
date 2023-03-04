12709d3e63cb21bdb8e83e4b8bfe88acbe4e5d96
MemoRodz <gmo.rodriguez@gmail.com>
2023-03-04 12:29:19 -0600

    Update: SweetAlerts and validations in the form

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,17 +7,104 @@ import swal from 'sweetalert';
 const serviceEmail = import.meta.env.VITE_EMAILJS_SERVICE;
 const templateContactUs = import.meta.env.VITE_EMAILJS_TEMPLATE_CONTACTUS;
 const userId = import.meta.env.VITE_EMAILJS_USERID;
+const correoValidate = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i; //import.meta.env.VITE_REGEX_VALIDATE_EMAIL;
+
+export function validate(formData) {
+
+  let errors = {};
+  // console.log('Objeto formData: ', formData);
+  // console.log('Regex: ', (correoValidate.test(formData.email)))
+  if (formData.nombre.length <= 0) {
+    // console.log('Nombre vacío: ', formData.nombre.length)
+    errors.nombre = 'El nombre no debe estar vacío.';
+  }
+  else if (formData.nombre.length < 6) {
+    // console.log('Nombre largo mínimo: ', formData.nombre.length)
+    errors.nombre = 'El nombre debe ser de al menos 6 caracteres.';
+  }
+  else if (formData.nombre.length > 25) {
+    // console.log('Nombre largo máximo: ', formData.nombre.length)
+    errors.nombre = 'El nombre debe ser de máximo 25 caracteres.';
+  }
+  else if (!correoValidate.test(formData.email)) {
+    // console.log('Validar correo:', formData.email)
+    errors.email = 'El correo debe ser correo electrónico válido.';
+  }
+  else if (formData.mensaje.length === 0) {
+    // console.log('Mensaje largo mínimo: ', formData.mensaje.length)
+    errors.mensaje = 'El mensaje no debe estar vacío.';
+  }
+  else if (formData.mensaje.length > 1500) {
+    // console.log('Mensaje largo máximo: ', formData.mensaje.length)
+    errors.mensaje = 'El mensaje supera el máximo de 1,500 caracteres.';
+  }
+  // console.log('Que tiene errors: ', errors);
+  return errors;
+}
 
 function About() {
 
-  function enviarEmail(e) {
-    e.preventDefault();
-    console.log("Correo: ", e.target.email.value);
-    emailjs.sendForm(serviceEmail, templateContactUs, e.target, userId).then(res => {
-      swal("¡Gracias por tu comentario!", "¡Espera nuestra respuesta!", "success");
-      console.log(res);
-    })
+  // console.log('RegexMail: ', correoValidate);
+
+  const formDataInitialState = {
+    nombre: "",
+    email: "",
+    mensaje: "",
+  };
+  const [validacion, setValidacion] = React.useState("");
+  const [formData, setFormData] = React.useState(formDataInitialState);
+  const [errors, setErrors] = React.useState({
+    nombre: "",
+    email: "",
+    mensaje: "",
+  });
+
+  const [text, setText] = React.useState("");
+
+  function handleTextChange(e) {
+    setText(e.target.value);
   }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log("Ya en submit, correo: ", e.target.email.value);
+    for (const [key, value] of Object.entries(errors)) {
+      if (value.length !== 0) setValidacion(value);
+    }
+    try {
+      console.log(`Service: ${serviceEmail}, Template: ${templateContactUs}, userId: ${userId}`);
+      emailjs.sendForm(serviceEmail, templateContactUs, e.target, userId).then(res => {
+        setFormData(formDataInitialState);
+        swal("¡Gracias por tu comentario!", "¡Espera nuestra respuesta!", "success");
+        console.log(res);
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setErrors(
+      validate({
+        ...formData,
+        [name]: value,
+      })
+    );
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  // function enviarEmail(e) {
+  //   e.preventDefault();
+  //   console.log("Correo: ", e.target.email.value);
+  //   emailjs.sendForm(serviceEmail, templateContactUs, e.target, userId).then(res => {
+  //     swal("¡Gracias por tu comentario!", "¡Espera nuestra respuesta!", "success");
+  //     console.log(res);
+  //   })
+  // }
 
   return (
     <div className={styles.about}>
@@ -115,21 +202,41 @@ function About() {
       <div style={{ width: "40%", backgroundColor: "lightgrey", margin: "0 auto", padding: "10px" }}>
         <h1>Contáctanos</h1>
         <hr />
-        <form onSubmit={enviarEmail}>
+        <form onSubmit={(e) => {
+          handleSubmit(e);
+        }}>
           <div >
             <div >
               <label><b>Nombre: </b></label>
               <input type="text" style={{
                 width: "316px",
                 height: "30px",
-              }} id="nombre" name="nombre" />
+              }}
+                id="nombre"
+                name="nombre"
+                minLength='6'
+                maxLength='25'
+                placeholder='Tu nombre.'
+                required
+                value={formData.nombre}
+                onChange={handleInputChange}
+              />
+              {errors.nombre ? <div>{errors.nombre}</div> : null}
             </div>
             <div >
               <label><b>Email: </b></label>
               <input type="text" style={{
                 width: "316px",
                 height: "30px",
-              }} id="email" name="email" />
+              }}
+                id="email"
+                name="email"
+                placeholder='Introduce un correo electrónico válido.'
+                required
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+              {errors.email ? <div>{errors.email}</div> : null}
             </div>
           </div>
           <div className="form-group">
@@ -137,9 +244,20 @@ function About() {
             <textarea type="text" style={{
               width: "338px",
               height: "210px",
-            }} id="mensaje" name="mensaje"></textarea>
+            }}
+              id="mensaje"
+              name="mensaje"
+              placeholder='Deja tu mensaje de hasta 1,500 caracteres.'
+              value={text} onChange={handleTextChange}
+            />
+            <p>{text.length}/1500</p>
+            
           </div>
-          <button type="submit" style={{ width: "50%", margin: "0 auto", marginTop: "20px" }}>Enviar Correo</button>
+          {validacion ? <div>{validacion}</div> : null}
+          <button type="submit" 
+          style={{ width: "50%", margin: "0 auto", marginTop: "20px" }}
+          disabled={Object.keys(validate).length}
+          >Enviar comentario</button>
         </form>
       </div>
     </div>
