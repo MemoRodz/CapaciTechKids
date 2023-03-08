b31dc17791bdfe347147184cb2f268e8b0a64eae
Sandro <sandro_malca@usmp.pe>
2023-03-08 16:45:57 -0500

    reviews solo se van a mostrar 2 nada mas x curso

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -120,7 +120,7 @@ export default function Detail() {
                       <h3>Average Score</h3>
                     </div>
                   </div>
-                  {review.map(e =>
+                  {review.map((e,i) => i<2? 
                     <div className={styles.comment}>
                       <div className={styles.commenttop}>
                         <div className={styles.userstart}>
@@ -141,7 +141,7 @@ export default function Detail() {
                       </div>
                       <div className={styles.commentbotom}><h3>{e.Comment}</h3></div>
                       <div className={styles.hrcomment}><hr /></div>
-                    </div>
+                    </div>:null
                   )}
                 </div>
               </div>
@@ -173,7 +173,8 @@ export default function Detail() {
         <div className={styles.detail}>
           <img src={course.Image} alt="{course.Title}" />
           <h1>{course.Title}</h1>
-
+                <br />
+                <br />
           {userInfo.isLogged ?
             <>
               {userInfo.userRole !== "instructor" &&
