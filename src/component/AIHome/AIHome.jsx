8ce5dbd35940d7aab8da7a6ddc5ba5998263831a
Fabian Rizzi <faberizzi92@gmail.com>
2023-03-09 02:07:59 +0100

    cambios home, efectos en cards, ai prompting

---
+++ src/component/AIHome/AIHome.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './AIHome.module.css'
+import { Link } from 'react-router-dom'
+
+export default function AIHome() {
+  return (
+    <div className={styles.containerdiv}>
+        <Link to={'/pregunta'}>
+        <img className={styles.imagenrobot} src="https://res.cloudinary.com/dbbmgnhqf/image/upload/v1678317071/CAPACITECHKIDS/images/users/banner_curses/robotprofesor_thmqcq.png" alt="" />
+        </Link>
+        <div>
+        <div className={styles.textorobot} >En CapaciTechKids hemos creado un profesor virtual para que puedas realizarle consultas sobre cualquier tema de nuestros cursos. Él te explicará de manera didáctica y amigable, para que puedas resolver tus dudas a cualquier hora del día.</div>
+        <Link to={'/pregunta'}>
+        <div className={styles.preguntale} >PREGUNTALE A NUESTRO PROFESOR!</div>
+        </Link>
+        </div>
+    </div>
+  )
+}
+
--- src/component/index.js
+++ src/component/index.js
@@ -8,6 +8,8 @@ export { default as SortByScore } from './Filters/SortByScore/SortByScore';
 export { default as Footer } from './Layout/Footer/Footer';
 export { default as Layout } from './Layout/Layout';
 export { default as Nav } from './Layout/Nav/Nav';
+export { default as AIHome } from './AIHome/AIHome'
+export { default as CursosHome } from './CursosHome/CursosHome'
 export * from './Login/Login';
 export * from './Logout/Logout';
 export * from './Profile/Profile';
